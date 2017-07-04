177a771b3ed2070a5f6d14e9a1f953c682ada498
T- <T-@PC246>
2017-07-03 19:17:17 -0500

    CLASE 1 THREADS

---
+++ src/threads1/Application.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package threads1;
+
+/**
+ *
+ * @author T-
+ */
+public class Application {
+    
+    public static void main(String[] args) {
+        //Ciclo de vida del thread
+        
+        //Nace
+        PrimerThread t1 = new PrimerThread();
+        
+        //Lo ponemos en estado iniciado: estado Runnable
+        t1.start(); 
+        //El soguiente paso es meterlo a la cpu(esto lo hace el administrador de thread)
+        
+    }
+}
